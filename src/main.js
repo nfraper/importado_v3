c7ca73e5de10d18887a0585a4a12d140ec4977ca
nefrance <nerea0980@gmail.com>
2024-02-27 11:50:06 +0100

    Importando plantilla

--- src/main.js
+++ src/main.js
@@ -1,77 +1,49 @@
-require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css');
-
+const rutaArchivo = './index.html';
+ 
 var SDK = require('blocksdk');
 var sdk = new SDK(null, null, true); // 3rd argument true bypassing https requirement: not prod worthy
+ 
+fetch(rutaArchivo)
+  .then(response => response.text())
+  .then(htmlContent => {
+    // El contenido del archivo HTML está almacenado en la variable htmlContent
+ 
+    sdk.setContent(htmlContent);
+ 
+  })
+  .catch(error => console.error('Error al cargar el archivo:', error));
+ 
+  document.getElementById('enviar').addEventListener('click', () =>{
+ 
+    var persona = document.getElementById('dynamicSelect').value;
+    var fecha = document.getElementById('datepicker').value;
+    var hora = document.getElementById('timepicker').value;
+ 
+    var payload = JSON.stringify({
+      items: [{
+        "Nombre": persona,
+        "Fecha": fecha,
+        "Hora": hora
+      }]
+    });
+    console.log(payload);
 
-var address, width, height, zoom, link, mapsKey;
-
-function debounce (func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-}
-
-function paintSettings () {
-	document.getElementById('text-input-id-0').value = mapsKey;
-	document.getElementById('text-input-id-1').value = address;
-	document.getElementById('slider-id-01').value = width;
-	document.getElementById('slider-id-02').value = height;
-	document.getElementById('slider-id-03').value = zoom;
-}
-
-function paintSliderValues () {
-	document.getElementById('slider-id-01-val').innerHTML = document.getElementById('slider-id-01').value;
-	document.getElementById('slider-id-02-val').innerHTML = document.getElementById('slider-id-02').value;
-	document.getElementById('slider-id-03-val').innerHTML = document.getElementById('slider-id-03').value;
-}
-
-function paintMap() {
-	mapsKey = document.getElementById('text-input-id-0').value;
-	address = document.getElementById('text-input-id-1').value;
-	width = document.getElementById('slider-id-01').value;
-	height = document.getElementById('slider-id-02').value;
-	zoom = document.getElementById('slider-id-03').value;
-	link = document.getElementById('text-input-id-2').value;
-	if (!address) {
-		return;
-	}
-	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
-		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
-		'&markers=' + address.split(' ').join('+') + '&key=' + mapsKey;
-	sdk.setContent('<a href="' + link + '"><img src="' + url + '" /></a>');
-	sdk.setData({
-		address: address,
-		width: width,
-		height: height,
-		zoom: zoom,
-		link: link,
-		mapsKey: mapsKey
-	});
-	localStorage.setItem('googlemapsapikeyforblock', mapsKey);
-}
-
-sdk.getData(function (data) {
-	address = data.address || '';
-	width = data.width || 400;
-	height = data.height || 300;
-	zoom = data.zoom || 15;
-	link = data.link || '';
-	mapsKey = data.mapsKey || localStorage.getItem('googlemapsapikeyforblock');
-	paintSettings();
-	paintSliderValues();
-	paintMap();
-});
-
-document.getElementById('workspace').addEventListener("input", function () {
-	debounce(paintMap, 500)();
-	paintSliderValues();
-});
+ 
+    // URL de la página web
+          //const url = 'https://mc1-y847vx6hhqvmtnnxy67y87b1.rest.marketingcloudapis.com/data/v1/async/dataextensions/key:Pruebas_Insercion_Datos_API/rows';
+ 
+          fetch('https://mc1-y847vx6hhqvmtnnxy67y87b1.rest.marketingcloudapis.com/data/v1/async/dataextensions/key:Pruebas_Insercion_Datos_API/rows', {
+            method: 'POST',
+            body: payload,
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IjQiLCJ2ZXIiOiIxIiwidHlwIjoiSldUIn0.eyJhY2Nlc3NfdG9rZW4iOiJaMms5bHdhSkJqQ0NFVHE3V3Rkb1dmbFgiLCJjbGllbnRfaWQiOiJ2Y25ld2t1M2k5NG5ubW56MXJrdzFtb3EiLCJlaWQiOjEwMDAxNDI3MCwic3RhY2tfa2V5IjoiUzEwIiwicGxhdGZvcm1fdmVyc2lvbiI6MiwiY2xpZW50X3R5cGUiOiJTZXJ2ZXJUb1NlcnZlciIsInBpZCI6NjkyfQ.7BUgheQomsHVQNMteXuTrDVuEaCSu_1k__7nlANlZYE.lc8yFfXkaXWeDKvb1AsHXprNmfBiQpOZMw4CrLT8H0ebdg3k87uw_O7DVfXW_31Uu6dnH_2Rt1I7mOHn4r3Aou1kP13-ImvfHJwBnuyewzSQrvAMlABJCr-6qVrZyRowArMIcb8gVpk8iAJgjxhmndWtVjPNtA3tGzSeQ"
+            }
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('La solicitud falló con el código: ' + response.status);
+            }
+            return response.json();
+          })
+})
